a18ed6582f311f99d204239db79ac2be4e93ae5e
Cristofer Reyes <creyesl5@miumg.edu.gt>
2025-02-26 19:27:54 -0600

    Prueba Cris

---
+++ Prueba01/src/main/java/com/mycompany/prueba01/Prueba01.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.prueba01;
+
+/**
+ *
+ * @author abics
+ */
+public class Prueba01 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        int a=5;
+        int b=15;
+        int c=a+b;
+        System.out.println("total: "+c);
+    }
+}
